e05354ddfe8ad3a2c398f82602c142d1c97d6950
Raúl Dávila Martínez solares <rdvmtz@gmail.com>
2015-11-30 09:37:47 -0600

    File

--- user/retirocheques.h
+++
@@ -1,8 +0,0 @@
-/*
-
-*/
-
-void retirocheques(struct info_usuario usuario[NUMUSUARIOS], struct info_sucursal sucursal[NUMSUCURSALES], struct info_bitacora bitacora[NUMREGISTROS], int num_usuarios, int num_sucursales, int num_registros) {
-
-  
-} // FUNC AGREGAR
